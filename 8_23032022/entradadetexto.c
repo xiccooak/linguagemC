2bd20863d8d7190a084b05f7ad45220f240a70f7
Usuariofr <chicofasc@gmail.com>
2022-03-25 09:17:43 -0300

    criando o arquivo README

---
+++ 8_23032022/entradadetexto.c
@@ -0,0 +1,20 @@
+/* Ler entrada de texto */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#void main()
+
+{  
+
+
+char(produto) [30];
+printf("informe o nome do produto:\n");
+scanf("%s" , &produto);
+
+printf("produto: %s produto);
+
+}
+
+
+

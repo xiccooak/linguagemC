2eed5e2e294970b36394c05b17dd10c65dcc6674
Usuariofr <chicofasc@gmail.com>
2022-04-01 09:12:24 -0300

    array2

---
+++ 12-31032022/array1.c
@@ -0,0 +1,11 @@
+/*Prograna para trabalhar com o primerio array*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void main (){
+
+    int n[5] = {10,5,6,11,2};
+    printf("%d\n",n[1]);
+
+
+}
---
+++ 12-31032022/array2.c
@@ -0,0 +1,16 @@
+/*Programa que realiza usando array*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "calculos.h"
+
+void main(){
+   
+    int v[10]={1,23,52,4,0,1,2,5,8,10};
+   
+       soma(v,10);
+       maiorValor(v,10);
+       menorValor(v,10);
+       ordenar(v,10);
+       
+}
